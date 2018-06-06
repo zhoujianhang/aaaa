44a638d430e2c5adfac3e736185f0d93bbc04801
zhoujianhang <39951064+zhoujianhang@users.noreply.github.com>
2018-06-06 13:51:54 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/admin/day605/MainActivity.java
+++ app/src/main/java/com/example/admin/day605/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //初始界面
-        //123
+        //1231
         setContentView(R.layout.activity_main);
     }
 }
